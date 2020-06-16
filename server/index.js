d525e91e356e561d1b758ee82503458bca5555ff
Richard Doe <richard.doe@rwdit.net>
2020-06-16 17:37:47 +0100

    Timeout on server requests

--- server/index.js
+++ server/index.js
@@ -2,16 +2,22 @@ const consola = require('consola');
 
 const express = require('express');
 const morgan = require('morgan');
+const timeout = require('connect-timeout');
 const { Nuxt, Builder } = require('nuxt');
 const app = express();
 const host = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+app.use(timeout('1s'));
 app.set('port', port);
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js');
 config.dev = !(process.env.NODE_ENV === 'production');
+
+const haltOnTimedout = (req, res, next) => {
+  if (!req.timedout) next();
+};
 
 async function start() {
   // Init Nuxt.js
@@ -74,6 +80,7 @@ async function start() {
 
   // Give nuxt middleware to express
   app.use(nuxt.render);
+  app.use(haltOnTimedout);
 
   // Listen the server
   app.listen(port, host);
